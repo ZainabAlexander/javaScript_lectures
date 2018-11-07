0da032f0d5803c9eb0b9c0edd40f7ba63cb9541a
Zainab <zainabalexander@gmail.com>
2018-11-07 11:38:41 +0200

    uploading first javaScript lecture

---
+++ lecture 7.11.18/js/main.js
@@ -0,0 +1,104 @@
+
+
+// Variables
+
+var message = 'Good Morning';
+    // message = 'how are you?';
+   
+
+// creating alerts
+
+// alert(age);
+// alert(gender);
+// alert(name);
+
+// Creating console logs
+
+// console.log(age);
+// console.log(gender);
+// console.log(name);
+
+// Creating prompts.
+
+var name = prompt('Enter your name here');
+alert("hello, " + name);
+
+
+// Always remember to close a string with the same type of quotation marks you started it with
+
+//Operators:
+
+// + addition
+// - subtraction
+// / division
+// * multiplication 
+
+// the + operator can be applied to two strings values. This will join two strings together, otherwise called concatenation.
+// in the console 'Hey' + ' there '
+
+// whats a condition?
+
+//
+
+// recipe.
+
+// begin
+// get out the rice cooker
+// fill it with rice
+// fill it with water 
+// cook the rice
+
+// Variables
+// var start = 'fill it';
+// var white = 'rice';
+// var wrap = 'tortilla';
+
+
+
+// Concatination: example
+
+var name = 'Zainab';
+    age = 22;
+    gender = 'female';
+    surname = 'surname';
+    studies = 'coding';
+    address = '18 primrose street belhar';
+    institute = 'Life Choices Academy';
+
+    console.log("My name is " + name + " " + surname + " I am  a " + gender + " I live in " + address + " I currently study " + studies + " at the " + institute + ' in lansdowne.')
+
+
+    var mommy = 'Zuleiga';
+        old= 50;
+        gender = 'female';
+        surname= 'alexander';
+        work = 'teacher';
+        address = '18 primrose street belhar';
+        place = 'Modderdam High School';
+
+    console.log("My mothers name is " + mommy + " " + surname+ " She is  a " + gender + " she lives at " + address + " she currently is a  " + work + " at  " + institute + ' in Bontheweul.')
+
+    var name= "ishmail Sadien";
+        live= '3 adrian road'
+        
+     console.log( "my boyfriends name is " + name + " he lives at " + live )
+     console.log( " i love " + name)
+
+     var name= 'ishmail';
+         father= 'daddy';
+
+console.log( "my name is " + name + " and i am Zainab's " + father)
+
+var name = 'yaseen Alexander';
+
+console.log("my brothers name is " + name)
+
+var name = '18 children';
+
+console.log("i have " + name)
+
+
+
+
+
+
